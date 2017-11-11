e50e30b2f7065c0580bc73d63a7d2e95584b3be9
William Stevens <contact@wastevensv.com>
2017-11-10 20:29:41 -0500

    Renamed emit to dot.

--- src/lib.c
+++ src/lib.c
@@ -36,7 +36,7 @@ void do_size(afi_Entry *self, afi_State *state) {
 	puts(str);
 }
 
-void do_emit(afi_Entry *self, afi_State *state) {
+void do_dotd(afi_Entry *self, afi_State *state) {
 	char str[16];
 	sprintf(str, "%d", PEEK(state->args));
 	puts(str);
@@ -47,20 +47,22 @@ void do_pop(afi_Entry *self, afi_State *state) {
 }
 
 afi_Node *lib_initDict(afi_Node *dict) {
-	afi_Entry *add  = afi_defEntry("+", do_add, 0);
-	afi_Entry *sub  = afi_defEntry("-", do_sub, 0);
-	afi_Entry *mul  = afi_defEntry("*", do_mul, 0);
-	afi_Entry *div  = afi_defEntry("/", do_div, 0);
+	afi_Entry *add  = afi_defEntry("+",    do_add,  0);
+	afi_Entry *sub  = afi_defEntry("-",    do_sub,  0);
+	afi_Entry *mul  = afi_defEntry("*",    do_mul,  0);
+	afi_Entry *div  = afi_defEntry("/",    do_div,  0);
 	afi_Entry *size = afi_defEntry("SIZE", do_size, 0);
-	afi_Entry *emit = afi_defEntry("EMIT", do_emit, 0);
-	afi_Entry *pop  = afi_defEntry("POP", do_pop, 0);
+	afi_Entry *dot  = afi_defEntry(".",    do_dotd, 0);
+	afi_Entry *dotd = afi_defEntry(".D",   do_dotd, 0);
+	afi_Entry *pop  = afi_defEntry("POP",  do_pop,  0);
 
 	dict = afi_addEntry(dict, add);
 	dict = afi_addEntry(dict, sub);
 	dict = afi_addEntry(dict, mul);
 	dict = afi_addEntry(dict, div);
 	dict = afi_addEntry(dict, size);
-	dict = afi_addEntry(dict, emit);
+	dict = afi_addEntry(dict, dot);
+	dict = afi_addEntry(dict, dotd);
 	dict = afi_addEntry(dict, pop);
 
 	return dict;
